19c4125c55a56c8effb154d8aa6f330433b2950c
ShinkuWRLD <91349393+ShinkuWRLD@users.noreply.github.com>
2024-02-13 17:12:13 -0500

    had it answer a question and return a prompt

--- openai-test.mjs
+++ openai-test.mjs
@@ -1,19 +1,22 @@
-import dotenv from "dotenv";
-dotenv.config(); // Load environment variables from .env file
-
-const apiKey = process.env.OPENAI_API_KEY;
-
 import OpenAI from "openai";
-
+const apiKey = process.env.OPENAI_API_KEY;
 const openai = new OpenAI(apiKey);
 
 async function main() {
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
-    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are a helpful assistant designed to output JSON.",
+      },
+      { role: "user", content: "How did Michael Jackson die?" },
+    ],
+    model: "gpt-3.5-turbo-0125",
+    response_format: { type: "json_object" },
   });
 
-  console.log(completion.choices[0]);
+  // Extract and log just the content
+  console.log(completion.choices[0].message.content);
 }
 
 main();
